770637463d57b1060ae221edafb6edc3db356420
aboobacker <abubackermax10@gmail.com>
2026-07-01 21:01:29 +0530

    Time: 4 ms (25.00%) | Memory: 56 MB (73.53%) - LeetSync

---
+++ 2819-remove-trailing-zeros-from-a-string/remove-trailing-zeros-from-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} num
+ * @return {string}
+ */
+var removeTrailingZeros = function(num) {
+    num.toString()
+    let x =0
+    for(let i=num.length-1;i>=0;i--){
+        if(num[i]!=="0"){
+            x = i;
+            break;
+
+        }
+    }
+    return num.slice(0,x+1)
+};
