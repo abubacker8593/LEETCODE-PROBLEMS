6cdbfcf51a362633a88ae4d334121faf93c53ff2
aboobacker <abubackermax10@gmail.com>
2026-07-04 16:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 53.5 MB (90.32%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,19 +6,10 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    
-    let arr = [...nums1.splice(0,m),...nums2];
-
-    
-    
-    for(let i = 0;i<arr.length;i++){
-        for(let j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                [arr[j],arr[j+1]] = [arr[j+1],arr[j]]
-            }
-        }
-    }
-    for(z=0;z<arr.length;z++){
-        nums1[z]=arr[z]
-    }
+       for(let i = 0; i<n; i++){
+    nums1[m+i]=nums2[i];
+   }
+   nums1.sort((a, b) => a - b);
 };
+
+
