e369af77d7089cbef1819fedad20a0dacffb368e
aboobacker <abubackermax10@gmail.com>
2026-07-07 12:41:43 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (54.46%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -4,10 +4,7 @@
  */
 var longestCommonPrefix = function(strs) {
     let prefix = strs[0]
-    let x =prefix.length
-
     for(let i =1;i<strs.length;i++){
-        let prefi = true
         while(!strs[i].startsWith(prefix)){
                 prefix =prefix.slice(0,-1)
         }
