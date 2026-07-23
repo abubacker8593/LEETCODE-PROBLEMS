c4e506f16ab3a65875c61ccdcf9c7e032de16363
aboobacker <abubackermax10@gmail.com>
2026-07-24 02:34:10 +1100

    Time: 3 ms (37.94%) | Memory: 60 MB (67.20%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -7,7 +7,7 @@ var moveZeroes = function(nums) {
     for(let i=0;i<nums.length;i++){
         if(nums[i]!=0){
             [nums[i],nums[j]] = [nums[j],nums[i]];
-            j = j+1;
+            j++;
         }
         
     }
