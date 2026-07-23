a27d84dbde368a2d8dbea38ccfd3eae22251b8a4
aboobacker <abubackermax10@gmail.com>
2026-07-24 03:20:06 +1100

    Time: 1 ms (72.64%) | Memory: 54.5 MB (90.95%) - LeetSync

---
+++ 136-single-number/single-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let single = 0
+    for(let i=0;i<nums.length;i++){
+        single = single ^ nums[i]
+
+
+    }
+    return single
+    
+    
+};
