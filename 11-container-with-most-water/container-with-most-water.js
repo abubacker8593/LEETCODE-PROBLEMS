2d1ddc66d13125308bc2e00f690b03b83607ef5a
aboobacker <abubackermax10@gmail.com>
2026-07-29 21:34:02 +0530

    Time: 963 ms (5.47%) | Memory: 76.1 MB (5.12%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    area =0
+    let right = height.length-1;
+    let left=0
+    while(left<right){
+        let x = Math.min(height[left],height[right])*Math.abs(left-right)
+        console.log(x)
+        if(x>area){
+            area = x
+        }
+        if(height[left]>height[right]){
+            right--
+        }else{
+            left++
+        }
+    
+}
+    return area
+}
+    
